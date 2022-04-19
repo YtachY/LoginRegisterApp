21d2a23a2664a0b94d42fee544dd1d3afb8884e5
dodochi <dodongchien1232@gmail.com>
2022-04-20 00:26:16 +0700

    [Updated] Added logic SQLite connector

--- app/src/main/java/vn/nhom5/lab04/data/model/LoggedInUser.java
+++ app/src/main/java/vn/nhom5/lab04/data/model/LoggedInUser.java
@@ -3,21 +3,53 @@ package vn.nhom5.lab04.data.model;
 /**
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
+
 public class LoggedInUser {
 
     private String userId;
-    private String displayName;
+    private String name;
+    private String userEmail;
+    private String password;
 
-    public LoggedInUser(String userId, String displayName) {
+    public LoggedInUser() {
+    }
+
+    public LoggedInUser(String userId, String name, String userEmail, String password) {
         this.userId = userId;
-        this.displayName = displayName;
+        this.name = name;
+        this.userEmail = userEmail;
+        this.password = password;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public String getDisplayName() {
-        return displayName;
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- app/src/main/java/vn/nhom5/lab04/ui/register/RegisterActivity.java
+++ app/src/main/java/vn/nhom5/lab04/ui/register/RegisterActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import vn.nhom5.lab04.R;
+import vn.nhom5.lab04.databinding.ActivityLoginBinding;
+import vn.nhom5.lab04.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private ActivityRegisterBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
